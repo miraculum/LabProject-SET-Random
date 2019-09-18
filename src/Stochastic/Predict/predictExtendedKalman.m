7d324b1b9cb5eb42655073ad053eb12b764cfaaf
miraculum <manuelbelchior5@gmail.com>
2019-09-18 17:07:20 +0100

    04

--- src/Stochastic/Predict/predictExtendedKalman.m
+++ src/Stochastic/Predict/predictExtendedKalman.m
@@ -1,30 +1,19 @@
     
-%PREDICTEXTENDEDKALMAN First order Extended Kalman predict
-%
-% Syntax:
-%   [x,P] = PREDICTEXTENDEDKALMAN(x,P,u,f,Q,w)
-%
+                        % Extended Kalman predict %
+
 % In:
 %   x - mean state estimate of previous step
 %   P - state covariance of previous step
 %   u - control vector
-%   f - xean prediction E[f(x[k-1],q=0)] function handle. E.g. f = @(x,u) x + log(x) + u
-%   Q - Process noise covariance matrix              (optional, default zero)
-%   w - process noise                                (optional, default zero)
-%   f1- first derivative of f in the form of function handle.        (calculated on the first run)
-
-%   w - Process noise                                (optional, default zero)
+%   f - mean prediction E[f(x[k-1],q=0)] function handle. 
+%        E.g. f = @(x,u) x + log(x) + u
+%   f1- first derivative of f in the form of function handle.
+%   Q - Process noise covariance matrix            (optional, default zero)
+%   w - process noise                              (optional, default zero)
 %
 % Out:
 %   x - Updated state mean
 %   P - Updated state covariance
-%   
-% Description:
-%   Perform first order Extended Kalman Filter prediction step.
-%
-% See also:
-% updateExtendedKalmam
-
 
 
 function [x,P,f1] = predictExtendedKalman(x,P,u,f,Q,f1)
@@ -46,9 +35,6 @@ function [x,P,f1] = predictExtendedKalman(x,P,u,f,Q,f1)
     
 
         %Matrix size test
-        %F- nxm
-        %P- mxm
-        %Q- nxn
 
         if size(F,2) ~= size(P,1) && size(F,2) ~= size(P,2) && size(Q,1) ~= size(F,1) && size(Q,2) ~= size(F,1)
             errorMessage("matrix size error in predictExtendedKalman");
--- src/Stochastic/Predict/predictKalman.m
+++ src/Stochastic/Predict/predictKalman.m
@@ -1,8 +1,34 @@
-%TODO: verificar tamanho das matrizes
-%      Há parâmetros que têm de ser passados num vetor
- 
+                      % Kalman Filter predict %
+                      
+% In:
+%   x - Nx1 mean state estimate of previous step
+%   P - NxN state covariance of previous step
+%   F - Transition matrix of discrete model (optional, default identity)
+%   B - Input effect matrix                 (optional, default identity)
+%   u - Constant input                      (optional, default empty)
+%   Q - Process noise of discrete model     (optional, default zero)
+
+% Out:
+%   X - Predicted state mean
+%   P - Predicted state covariance
+
 function [x, P] = predictKalman(P, x, F, B, u, Q)
 
+  % Number of inputs control 
+  if nargin < 3
+    F = [];
+  end
+  if nargin < 4
+    B = [];
+  end
+  if nargin < 5
+    u = [];
+  end
+  if nargin < 6
+    Q = [];
+  end
+
+  % Apply defaults
     if isempty(F)
         F = eye(size(x,1));
     end
@@ -10,19 +36,16 @@ function [x, P] = predictKalman(P, x, F, B, u, Q)
     if isempty(B)
         B = eye(size(x,1),size(u,1));
     end
-  
-    ft = F';
     
     if isempty(Q)
-        Q = eye(size(ft,1));
+        Q = eye(size(F',1));
     end
-
 
     %predicted state estimate
     x = F * x + B * u;
          
     %predicted error covariace
-    P = F * P * ft + Q;
+    P = F * P * F' + Q;
     
 
     
